abd5cc8d5ebaefd7ed92fb3c410e9ef1eebd14b2
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2022-09-20 13:38:00 -0300

    Adição de alerts

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,39 @@
 const form = document.querySelector('.form-add-todo')
-const ul = document.querySelector('.todos-container')
-const alerta = document.querySelector('.alerta')
+const container = document.querySelector('.todos-container')
+const alerta = document.querySelector('.alert-danger')
+const alertSuccess = document.querySelector('.alert-success')
+
+/* Função Error */
+const funcError = () => {
+    alerta.classList.remove('d-none')
+    setTimeout(() => {
+        alerta.classList.add('d-none')
+    }, 3000);
+}
+
+/* Função Success */
+const funcSuccess = () => {
+    alertSuccess.classList.remove('d-none')
+    setTimeout(() => {
+        alertSuccess.classList.add('d-none')
+    }, 3000);
+    form.reset()
+}
 
 form.addEventListener('submit', event => {
     event.preventDefault()
     const inputValue = event.target.add.value.trim()
-
-    if (inputValue.length != 0) {
-        ul.innerHTML += `
+    if (inputValue.length === 0) {
+        funcError()
+    } else {
+        funcSuccess()
+        container.innerHTML += `
         <li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${inputValue}</span>
         <i class="far fa-trash-alt delete"></i>
-      </li>    
-      `
-
-    } else {
-        alerta.classList.remove('d-none')
-        setTimeout(sumirAlerta, 2000)
+        </li>
+        `
     }
-    function sumirAlerta() {
-        alerta.classList.add('d-none')
-    }
-
-
-    event.target.reset()
-
-
+    console.log(inputValue)
 })
+
